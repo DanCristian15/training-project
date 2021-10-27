f37fc518f28ce01c6c4bac6e073f38a427484ea4
Dan Cristian <cristian.dan@boatyardx.com>
2021-10-27 14:39:56 +0300

    training step 1

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+      <h1>Project components</h1>
+      <buton 
+      :text="btnText" 
+      :btnHeight="100" 
+      :btnWidth="190"
+      :dark="darkmode"
+      @eventEmited="alertMethod( )"
+      >
+        <p><font-awesome-icon icon="coffee" /></p>
+      </buton>
+  </div>
+</template>
+
+
+<script>
+
+import Button from './Button.vue'
+
+
+export default {
+  data( ) {
+    return {
+      btnText: "click me",
+      darkmode: true
+    }
+  },  
+  components: {
+    buton: Button
+  },
+  methods: {
+    alertMethod( ) {
+      alert( "alert" );
+    }
+  }
+}
+</script>
+
+<style >
+ 
+</style>
---
+++ src/Button.vue
@@ -0,0 +1,61 @@
+<template>
+<div>
+<button 
+:style="{ 'height': btnHeight + 'px', 'width': btnWidth + 'px' }"
+:class='!dark ? "lightmode" : "darkmode" '
+@click="eventEmit"
+>
+  <slot></slot><p> {{text}} </p><slot></slot>
+</button>
+</div>
+
+</template>
+
+<script>
+
+
+
+export default { 
+    props: {
+        text: {
+            type: String,
+            require: true
+        },
+        btnHeight: {
+            default: 20
+        },
+        btnWidth: {
+            default: 100
+        },
+        dark: {
+            type: Boolean
+        }
+    },
+    methods: {
+       eventEmit( ) {
+           this.$emit( 'eventEmited' , {
+               alertmessage: 'mesaj'
+           })
+       }
+    }
+}
+</script>
+<style scoped>
+
+.darkmode {
+    background-color: black;
+    color: white;
+}
+
+.lightmode {
+    background-color: rgb(255, 255, 255);
+    color: black;
+}
+
+p {
+    display: inline-block;
+    margin: 5px;
+}
+
+
+</style>
